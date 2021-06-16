b0e3d055dcd3e5250384421dc13c46b322f303d9
Adam-M93 <adam.matthews93@gmail.com>
2021-06-16 15:12:05 +0100

    Add files via upload

---
+++ Coagulated def to FS txt.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import re
+import string
+
+
+droppedFile = sys.argv[1]
+
+#x = open(droppedFile)
+with open (droppedFile) as x:
+    fileList = x.readlines()
+    a = 0
+while a < len(fileList):
+    fileList[a] = fileList[a].strip('\n')
+    a += 1
+
+newFileName = (os.path.basename(sys.argv[1]))
+newFile = open(newFileName.split('.')[0] + '.txt', "w+")
+
+
+
+position = 0
+
+while position < len(fileList):
+    line = fileList[position]
+
+    if re.search('voxel', line, re.I):
+        #tilenumber = line[line.find('tile ')+1:line.find(' ')]
+        if re.search('rotate', line, re.I):
+            tilenumber = line[line.find('e ')+1:line.find(' r')]
+            newline = ('Voxel' + tilenumber + ' { filename ' + line.split(' ')[1].strip() + " rotate TRUE }" )
+        else:
+            tilenumber = line[line.find('e ')+1:line.find(' }')]
+            newline = ('Voxel' + tilenumber + ' { filename ' + line.split(' ')[1].strip() + " }" )
+        fileList[position] = newline
+    
+    position += 1
+
+
+position = 0
+#write everything to file
+while position < len(fileList):
+    newFile.write(fileList[position] + "\n")
+    position += 1
+
+
+os.system("pause")
