0247029dd43c2cf878b50687bb73d5b583d5ba3b
Adam-M93 <adam.matthews93@gmail.com>
2021-07-19 14:54:36 +0100

    Supports setting randomized voxel angle
    also updated settings.ini format, will auto update old format to new format

--- Blood def to FS txt.py
+++ Blood def to FS txt.py
@@ -20,10 +20,30 @@ def write_file():
 	config.write(open('Settings.ini', 'w'))
 
 config = configparser.RawConfigParser()
+config.optionxform = str
+
+#if old settings format exists, update to new format
+if os.path.exists(".\Settings.ini"):
+	config.read(".\Settings.ini")
+	if config.has_section('BlacklistedVoxels') and config.has_section('FSEditedVoxels'):
+		OLDBlacklistGet = config.get('BlacklistedVoxels', 'Blacklist', fallback='')
+		OLDFSVoxelsGet = config.get('FSEditedVoxels', 'FSEdit', fallback='')
+		OLDSections = config.sections()
+		for Section in OLDSections:
+			y = int(OLDSections.index(Section))
+			config.remove_section(OLDSections[y])
+		config.add_section('Settings')
+		config.set('Settings','Blacklist', OLDBlacklistGet)
+		config.set('Settings','AltFSVoxels', OLDFSVoxelsGet)
+		config.set('Settings','RandomizedAngle', '')
+		write_file()
+
 #if settings file doesn't exist create a blank one
 if not os.path.exists('Settings.ini'):
-	config['BlacklistedVoxels'] = {'Blacklist': ''}
-	config['FSEditedVoxels'] = {'FSEdit': '' }
+	config.add_section('Settings')
+	config.set('Settings','Blacklist', '')
+	config.set('Settings','AltFSVoxels', '')
+	config.set('Settings','RandomizedAngle', '')
 	write_file()
 
 config.read(".\Settings.ini")
@@ -33,15 +53,19 @@ position = 0
 while position < len(fileList):
 	line = fileList[position]
 	if os.path.exists(".\Settings.ini"):
-		BlacklistGet = config.get('BlacklistedVoxels', 'Blacklist', fallback='')
+		BlacklistGet = config.get('Settings', 'Blacklist', fallback='')
 		Blacklist = BlacklistGet.split(', ')
-		FSVoxelsGet = config.get('FSEditedVoxels', 'FSEdit', fallback='')
+		FSVoxelsGet = config.get('Settings', 'AltFSVoxels', fallback='')
 		FSVoxels = FSVoxelsGet.split(', ')
+		RandomizedAnglesGet = config.get('Settings', 'RandomizedAngle', fallback='')
+		RandomizedAngles = RandomizedAnglesGet.split(', ')
 	else:
 		BlacklistGet = ("")
 		Blacklist = ("")
 		FSVoxelsGet = ("")
 		FSVoxels = ("")
+		RandomizedAnglesGet = ("")
+		RandomizedAngles = ("")
 
 	if re.search('voxel', line, re.I):
 		tilenumber = re.findall(r'\b\d+\b', line)
@@ -52,14 +76,20 @@ while position < len(fileList):
 			y = int(tilenumber.index(x))
 			if tilenumber[y] in Blacklist:
 				blacklisted = ("// ")
-			if not tilenumber[y] in Blacklist:
+			else:
 				blacklisted = ("")
 				
 			#For Voxels that need modifications for FS
 			if tilenumber[y] in FSVoxels:
 				FSVoxel = ("_FS")
-			if not tilenumber[y] in FSVoxels:
+			else:
 				FSVoxel = ("")
+				
+			#For setting the Voxel to be at a randomized angle
+			if tilenumber[y] in RandomizedAngles:
+				RandomizedAngle = (" bRandomizedAngle TRUE")
+			else:
+				RandomizedAngle = ("")
 
 		#For Voxels that rotate
 		if ' rotate ' in line:
@@ -67,7 +97,7 @@ while position < len(fileList):
 		else:
 			Rotate = ""
 			
-		newline = (blacklisted + 'Voxel ' + tilenumber[0] + ' { filename ' + line.split(' ')[1].strip()[:-5] + FSVoxel + '.kvx"' + Rotate + " }" + line.split('}')[1] )
+		newline = (blacklisted + 'Voxel ' + tilenumber[0] + ' { filename ' + line.split(' ')[1].strip()[:-5] + FSVoxel + '.kvx"' + Rotate + RandomizedAngle + " }" + line.split('}')[1] )
 		
 		fileList[position] = newline
 		
@@ -77,18 +107,19 @@ while position < len(fileList):
 				y = int(tilenumber.index(x))
 				
 				#For some reason having "Voxel" causes this to break
-				fileList.insert(position + y, (blacklisted + 'Voxe ' + tilenumber[y] + ' { filename ' + line.split(' ')[1].strip()[:-5] + FSVoxel + '.kvx"' + Rotate + " }" + line.split('}')[1] ))
+				fileList.insert(position + y, (blacklisted + 'Voxe ' + tilenumber[y] + ' { filename ' + line.split(' ')[1].strip()[:-5] + FSVoxel + '.kvx"' + Rotate + RandomizedAngle + " }" + line.split('}')[1] ))
 
 	position += 1
-
 
 position = 0
 
 if not (BlacklistGet == ("")):
 	print('Blacklisted Voxels: ' + BlacklistGet)
 if not (FSVoxelsGet == ("")):
-	print('FS Edited Voxels: ' + FSVoxelsGet)
-
+	print('Alternate FS Voxels: ' + FSVoxelsGet)
+if not (RandomizedAnglesGet == ("")):
+	print('Randomized Voxel Angles: ' + RandomizedAnglesGet)
+	
 #Replace Voxe with Voxel
 fileList = [item.replace("Voxe ", "Voxel ") for item in fileList]
 	
